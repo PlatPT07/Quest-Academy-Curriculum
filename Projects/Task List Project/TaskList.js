565660243997a44f68217f5594aa0d5e7cfdcf86
PlatPT07 <n0name4me777@gmail.com>
2024-07-15 18:24:07 +0700

    JavaScript
    Basic JavaScript

--- Projects/Task List Project/TaskList.js
+++ Projects/Task List Project/TaskList.js
@@ -1,17 +1,33 @@
-var tasklist = [1, 2, 3]
+var tasklist = ["English", "Thai", "Chinese"];
 
+//add
 function add(subject) {
     tasklist.push(subject);
 }
 
+//remove
 function remove() {
-let remove= tasklist.shift();
-
-    console.log(remove);
+    let remove = tasklist.shift();
 }
 
+//display
+function display_task() {
+    if (tasklist.length == "")
+        console.log("No Task");
+    if (tasklist.length > 1)
+        console.log("Task:");
+    if (tasklist.length > 0)
+        console.log("1. " + tasklist[0]);
+    if (tasklist.length > 1)
+        console.log("2. " + tasklist[1]);
+    if (tasklist.length > 2)
+        console.log("3. " + tasklist[2]);
+    if (tasklist.length > 3)
+        console.log("4. " + tasklist[3]);
+    console.log("Total: " + tasklist.length);
+}
 
-
-add(4);
+add("Math");
 remove();
 
+display_task();
